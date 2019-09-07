316c16acaa1a59dfa3e5ab16660e379c1cb37b86
philiptwcn <philiptwcn@gmail.com>
2019-09-07 23:20:42 +0800

    1

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { ContentfulService } from '../services/contentful.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Entry } from 'contentful';
@@ -13,6 +14,36 @@ import { Entry } from 'contentful';
 
 export class CarouselComponent implements OnInit {
   product: Entry<any>;
+
+  images = [1, 2, 3, 4, 5, 6, 7].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  paused = false;
+  showNavigationArrows = true;
+  unpauseOnArrow = true;
+  pauseOnIndicator = true;
+  pauseOnHover = true;
+  wrap = true;
+
+  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
 
 
   constructor(
